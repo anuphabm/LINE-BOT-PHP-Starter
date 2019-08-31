809742e55b94b33a8de22f9fb3320248ff0f4c7a
anuphab meekusol <anuphabm@gmail.com>
2019-08-31 19:41:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,6 +43,24 @@ if(!is_null($events['events'])){
 
 	    echo $result . "\r\n";
 
+			    // Make a POSt Request to messaging API to reply to sender
+	    $url = 'http://203.150.199.91:8080/poona-webapp/webapi/myresource';
+	    $data = [
+	    	'replyToken' => $replyToken,
+		'messages' => [$messages],
+		 'content' => $content,
+	    ];
+	    $post = json_encode($data);
+	    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	    $result = curl_exec($ch);
+	    curl_close($ch);
 	
 	}
     }
